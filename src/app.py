809be99a72880c06e90f2732c52b9a56b46bd987
naufal afif <naufalafif58@gmail.com>
2020-04-10 21:12:42 +0700

    reformat code

--- src/app.py
+++ src/app.py
@@ -4,42 +4,46 @@
 from downloader import imageDownloader
 import argparse
 import sys
-sys.tracebacklimit=0
+
+sys.tracebacklimit = 0
 
 parser = None
+
+
 class Cli:
-  def __init__(self):
-    self.arguments = None
-    self.init_parser()
-    self.execute()
-  
-  def  init_parser(self):
-    parser = argparse.ArgumentParser(description='Simple App that download all images from url')
-    parser.add_argument('-m', type=int,
-                        help='Maximun number of tries to download url')
-    parser.add_argument('-t', type=int,
-                        help='Request timeout time in second')
-    parser.add_argument('-s', type=str,
-                        help='Path to save downloaded images')
-    parser.add_argument('url',
-                        help='Required site url')
-    self.arguments = parser.parse_args()
+    def __init__(self):
+        self.arguments = None
+        self.init_parser()
+        self.execute()
 
-  def execute(self):
-    url = self.arguments.url
-    max_try = self.arguments.m
-    timeout = self.arguments.t
-    save_path = self.arguments.s
+    def init_parser(self):
+        parser = argparse.ArgumentParser(
+            description="Simple App that download all images from url"
+        )
+        parser.add_argument(
+            "-m", type=int, help="Maximun number of tries to download url"
+        )
+        parser.add_argument("-t", type=int, help="Request timeout time in second")
+        parser.add_argument("-s", type=str, help="Path to save downloaded images")
+        parser.add_argument("url", help="Required site url")
+        self.arguments = parser.parse_args()
 
-    app = imageDownloader(url)
+    def execute(self):
+        url = self.arguments.url
+        max_try = self.arguments.m
+        timeout = self.arguments.t
+        save_path = self.arguments.s
 
-    if max_try:
-      app.set_max_try(max_try)
-    if timeout:
-      app.set_timeout(timeout)
-    if save_path:
-      app.set_save_path(save_path)
+        app = imageDownloader(url)
 
-    app.download()
-    
+        if max_try:
+            app.set_max_try(max_try)
+        if timeout:
+            app.set_timeout(timeout)
+        if save_path:
+            app.set_save_path(save_path)
+
+        app.download()
+
+
 cli_app = Cli()
--- src/downloader.py
+++ src/downloader.py
@@ -4,8 +4,10 @@ import validators
 import urllib.request
 import os
 
+
 def fillUrlDomain(parent_url, image_url):
     return "{}{}".format(parent_url, image_url)
+
 
 class imageDownloader:
     def __init__(self, url=None, max_try=3, timeout=5, save_path=os.getcwd()):
@@ -13,9 +15,9 @@ class imageDownloader:
         self.max_try = max_try
         self.timeout = timeout
         self.save_path = save_path
-        
+
         self.initPath()
-    
+
     def set_max_try(self, value):
         self.max_try = value
 
@@ -28,7 +30,7 @@ class imageDownloader:
     def initPath(self):
         if not os.path.exists(self.save_path):
             os.makedirs(self.save_path)
-    
+
     def downloadImageByUrl(self, image_url, title=None):
         url_to_download = image_url
         if not validators.url(image_url):
@@ -45,31 +47,31 @@ class imageDownloader:
 
         urllib.request.urlretrieve(url_to_download, title)
 
-    def extractImageUrls(self,request_try=1):
+    def extractImageUrls(self, request_try=1):
         if not validators.url(self.url):
-            raise Exception('Url Invalid')
+            raise Exception("Url Invalid")
 
         if request_try == self.max_try:
-            raise Exception('You Have Reach The Maximum Request')
+            raise Exception("You Have Reach The Maximum Request")
         try:
             response = requests.get(self.url, timeout=self.timeout)
         except requests.exceptions.RequestException as e:
-            print('Url Request Failed', e)
+            print("Url Request Failed", e)
             extractImageUrls(request_try + 1)
 
         if response.status_code != 200:
-            raise Exception('Url Requests Failed, Invalid Code ',self.url)
+            raise Exception("Url Requests Failed, Invalid Code ", self.url)
         response_in_html = html.fromstring(response.text)
-        universal_image_path = '//img/@src'
+        universal_image_path = "//img/@src"
         image_list = response_in_html.xpath(universal_image_path)
 
         return image_list
-        
+
     def download(self):
         image_url_list = self.extractImageUrls()
         for image_url in image_url_list:
             try:
                 self.downloadImageByUrl(image_url)
-                print("Download Success ",image_url)
+                print("Download Success ", image_url)
             except:
-                print("Download Failed ",image_url)
+                print("Download Failed ", image_url)
