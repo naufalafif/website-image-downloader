d757b4bbbf7088bfe888ee4920091438bcddd9bc
naufal afif <naufalafif58@gmail.com>
2020-04-10 22:00:54 +0700

    update code stucture, to use config file etc

--- src/app.py
+++ src/app.py
@@ -1,49 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from downloader import imageDownloader
+from .downloader import imageDownloader
+from config import RequestTimeout, RequestMaxRetries, SaveDirectory
 import argparse
-import sys
-
-sys.tracebacklimit = 0
-
-parser = None
-
 
 class Cli:
     def __init__(self):
         self.arguments = None
         self.init_parser()
-        self.execute()
 
     def init_parser(self):
         parser = argparse.ArgumentParser(
             description="Simple App that download all images from url"
         )
         parser.add_argument(
-            "-m", type=int, help="Maximun number of tries to download url"
+            "-m", type=int, help="Maximun number of tries to download url. default {}".format(RequestMaxRetries)
         )
-        parser.add_argument("-t", type=int, help="Request timeout time in second")
+        parser.add_argument("-t", type=int, help="Request timeout time in second. default {} seconds".format(RequestTimeout))
         parser.add_argument("-s", type=str, help="Path to save downloaded images")
         parser.add_argument("url", help="Required site url")
         self.arguments = parser.parse_args()
 
-    def execute(self):
+    def run(self):
         url = self.arguments.url
-        max_try = self.arguments.m
-        timeout = self.arguments.t
-        save_path = self.arguments.s
+        max_try = self.arguments.m if self.arguments.m else RequestMaxRetries
+        timeout = self.arguments.t if self.arguments.t else RequestTimeout
+        save_path = self.arguments.s if self.arguments.s else SaveDirectory
 
         app = imageDownloader(url)
-
-        if max_try:
-            app.set_max_try(max_try)
-        if timeout:
-            app.set_timeout(timeout)
-        if save_path:
-            app.set_save_path(save_path)
+        app .set_max_try(max_try)\
+            .set_timeout(timeout)\
+            .set_save_path(save_path)
 
         app.download()
-
-
-cli_app = Cli()
